086acb35c50c09d914aec91c382061e5820ad6a4
SethChloe <1015042407@qq.com>
2020-11-15 21:13:31 +0800

    -

---
+++ mypython/mypython_st/data-analysis.py
@@ -0,0 +1,8 @@
+import numpy as np 
+from pandas import Series,DataFrame
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+sns.set(style='whitegrid',palette="muted",color_codes=True)
+sns.despine(left=True,bottom=True)
+sns.set(color_codes=True)
---
+++ mypython/mypython_st/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='data-analysis',
+    version      ='2.0.1',
+    py_modules   =['data-analysis'],
+    author       ='long.ao',
+    author_email ='islong.ao@outlook.com',
+    description  ='This is my own data-analysis work-box.',
+    )
