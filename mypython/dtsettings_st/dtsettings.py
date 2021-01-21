894f498f5571199d1e06b49757e0e10a89ae0fae
SethChloe <1015042407@qq.com>
2021-01-21 14:19:54 +0800

    -

--- mypython/dtsettings_st/dtsettings.py
+++ mypython/dtsettings_st/dtsettings.py
@@ -83,3 +83,15 @@ class Apriori(object):
                         sorted(i.split(sign)))]
         final = final.T.sort_values(['confidence', 'support'], ascending=False)
         return final
+
+#方差分析 Wilcoxon符号秩检验
+def wilcoxon_signed_rank_test(samp, mu0=0):
+    temp = DataFrame(np.asarray(samp), columns=['origin_data'])
+    temp['D'] = temp['origin_data'] - mu0
+    temp['rank'] = abs(temp['D']).rank()
+    posW = sum(temp[temp['D'] > 0]['rank'])
+    negW = sum(temp[temp['D'] < 0]['rank'])
+    n = temp[temp['D'] != 0]['rank'].count()
+    Z = (posW - n * (n + 1) / 4) / np.sqrt((n * (n + 1) * (2 * n + 1)) / 24)
+    P = (1 - stats.norm.cdf(abs(Z))) * 2
+    return Z, P
